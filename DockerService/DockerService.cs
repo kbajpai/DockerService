f7919273d9c6006c661286590a868b015584e77c
Kunal Bajpai <kunal.bajpai@gmail.com>
2019-08-14 20:57:35 -0400

    Checkpoint

--- DockerService/DockerService.cs
+++ DockerService/DockerService.cs
@@ -1,14 +1,8 @@
 ﻿using CommonUtilities;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 
 namespace DockerService
